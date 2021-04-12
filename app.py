1baff3d9d71ac423339021b82b931f06577b71e2
Sakthivel Malligarjunan <63723301+sakthivelcyr@users.noreply.github.com>
2021-04-12 20:23:58 +0530

    Update app.py
    first commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Sakthivel Super Market')
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "<h1 align='center'>Sakthi Super Market</h1>"
+
+if __name__ == '__main__':
+    app.run()
