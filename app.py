f0564334d5d4039072986f499b047fb4d7740393
Sakthivel Malligarjunan <63723301+sakthivelcyr@users.noreply.github.com>
2021-04-13 08:52:48 +0530

    Update app.py
    first commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+import mysql.connector
 
+mydb = mysql.connector.connect(
+  host="ssm-mysql.mysql.database.azure.com",
+  user="ssm@ssm-mysql",
+  password="Sakthi@22",
+  autocommit=True,
+  database="ssm"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM login")
+
+myresult = mycursor.fetchall()
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Sakthi"
+    return "<h1 align='center'>Sakthi Super Market</h1>" + "<p>"+str(myresult)+"</p>"
 
 if __name__ == '__main__':
     app.run()
