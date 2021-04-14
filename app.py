1565dc77bda71ca01e85e125e3014539205b4fab
Sakthivel Malligarjunan <63723301+sakthivelcyr@users.noreply.github.com>
2021-04-14 17:45:22 +0530

    Update app.py
    first commit

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask
 import mysql.connector
 
-try:
-  mydb = mysql.connector.connect(
+#try:
+mydb = mysql.connector.connect(
     host="ssm-mysql.mysql.database.azure.com",
     user="ssm@ssm-mysql",
     password="Sakthi@22",
     autocommit=True,
     database="ssm"
   )
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM login")
+myresult = mycursor.fetchall()
 
-  mycursor = mydb.cursor()
+# except:
+#   myresult = "Database not connected"
 
-  mycursor.execute("SELECT * FROM login")
-
-  myresult = mycursor.fetchall()
-
-except:
-  myresult = "Database not connected"
 app = Flask(__name__)
 
 @app.route('/')
